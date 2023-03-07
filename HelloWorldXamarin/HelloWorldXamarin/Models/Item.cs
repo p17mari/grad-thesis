ba51310b7dce46b42d7184d5a49e9bc1bd233492
argir <argir@RO>
2023-03-07 18:13:43 +0200

    Add project files.

---
+++ HelloWorldXamarin/HelloWorldXamarin/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HelloWorldXamarin.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ HelloWorldXamarin/HelloWorldXamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HelloWorldXamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HelloWorldXamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
