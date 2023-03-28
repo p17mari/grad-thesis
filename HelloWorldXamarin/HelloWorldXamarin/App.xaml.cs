6ffe073dcd9484d1cfb4cff803229b1e18f6dfc9
p17mari <p17mari@ionio.gr>
2023-03-28 16:32:25 +0300

    finally deploying again

--- HelloWorldXamarin/HelloWorldXamarin/App.xaml.cs
+++ HelloWorldXamarin/HelloWorldXamarin/App.xaml.cs
@@ -1,37 +1,20 @@
-﻿using HelloWorldXamarin;
-using HelloWorldXamarin.Services;
-using HelloWorldXamarin.Views;
+﻿using HelloWorldXamarin.Services;
+using HelloWorldXamarin;
 using System;
-using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace formtest
+namespace HelloWorldXamarin
 {
     public partial class App : Application
     {
-        private static Database database;
-        public static Database Database
-        {
-            get { 
-                if (database == null)
-                {
-                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "formtest.db3"));
-                }
-                return database;
-            }
-        }
+
         public App()
         {
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
-        }
-
-        private void InitializeComponent()
-        {
-            throw new NotImplementedException();
         }
 
         protected override void OnStart()
